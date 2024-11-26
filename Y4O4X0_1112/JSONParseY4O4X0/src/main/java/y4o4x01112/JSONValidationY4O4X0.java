57e6bc40963544519e2260238315769d8180cdf6
SZKrisztián <szaloczy6@gmail.com>
2024-11-26 09:25:56 +0100

    json-validator

---
+++ Y4O4X0_1112/JSONParseY4O4X0/src/main/java/y4o4x01112/JSONValidationY4O4X0.java
@@ -0,0 +1,36 @@
+package y4o4x01112;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.everit.json.schema.Schema;
+import org.everit.json.schema.loader.SchemaLoader;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+public class JSONValidationY4O4X0 {
+
+	public static void main(String[] args) {
+		String schemaPath = "src/main/resources/orarendY4O4X0Schema.json";
+		String jsonPath = "src/main/resources/orarendY4O4X0.json";
+		{
+		
+		try(InputStream schemaStream = new FileInputStream(schemaPath);
+			InputStream jsonStream = new FileInputStream(jsonPath)) {
+			
+			JSONObject schemaJson = new JSONObject(new JSONTokener(schemaStream));
+			Schema schema = SchemaLoader.load(schemaJson);
+			
+			JSONObject json = new JSONObject(new JSONTokener(jsonStream));
+			
+			schema.validate(json);
+			System.out.println("A JSON érvényes a séma alapján");
+			
+		}catch(Exception e) {
+			System.out.println("Hiba történt a validáció során.");
+			System.out.println(e.getMessage());
+		}
+		}
+	}
+
+}
